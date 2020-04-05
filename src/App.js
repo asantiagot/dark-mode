ad98f7260b3895ea035dc7e6690aa9f9c7b35ee7
Jose Santiago <josantorres93@gmail.com>
2020-04-05 13:22:05 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './styles.css';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(getInitialMode);
+
+  useEffect(() => {
+    localStorage.setItem('dark', JSON.stringify(darkMode));
+  }, [darkMode]);
+
+  function getInitialMode() {
+    const darkMode = JSON.parse(localStorage.getItem('dark'));
+    return darkMode || false;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={darkMode ? "dark-mode" : "light-mode"}>
+      <nav>
+        <div className="toggle-container">
+          <button onClick={() => setDarkMode(prevMode => !prevMode)}>
+            Toggle Mode
+          </button>
+        </div>
+      </nav>
+      <main>
+        <h1>{darkMode ? "Dark Mode" : "Light Mode"}</h1>
+        <h2>Toggle the switch to see some magic happen!</h2>
+      </main>
     </div>
+
   );
 }
 
